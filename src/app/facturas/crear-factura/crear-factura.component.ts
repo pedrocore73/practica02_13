fd8ccef89b6222bea4da93701c4a054f42efe109
pedrocore73 <pjimenez@corenetworks.es>
2019-11-20 20:49:49 +0100

    search

--- src/app/facturas/crear-factura/crear-factura.component.ts
+++ src/app/facturas/crear-factura/crear-factura.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MensajesService } from 'src/app/servicios/mensajes.service';
+import { FacturasService } from 'src/app/servicios/facturas.service';
 
 @Component({
   selector: 'app-crear-factura',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearFacturaComponent implements OnInit {
 
-  constructor() { }
+  formFra: FormGroup;
+
+  constructor(private facturasService: FacturasService,
+              private router: Router,
+              private mensajesService: MensajesService) { }
 
   ngOnInit() {
+    this.formFra = new FormGroup({
+      cliente: new FormControl(null)
+    });
+    this.onSearch();
+  }
+
+  onSearch() {
+    this.formFra.get('cliente').valueChanges
+                      .subscribe(nombre =>{
+                        console.log(nombre);
+                      })
   }
 
 }
