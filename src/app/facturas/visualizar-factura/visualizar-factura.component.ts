204fdcc4561d23d5179e581a527e1858dd586956
pedrocore73 <pjimenez@corenetworks.es>
2019-11-22 18:40:34 +0100

    imprimir

--- src/app/facturas/visualizar-factura/visualizar-factura.component.ts
+++ src/app/facturas/visualizar-factura/visualizar-factura.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FacturasService } from 'src/app/servicios/facturas.service';
+import { NumerosService } from 'src/app/servicios/numeros.service';
 
 @Component({
   selector: 'app-visualizar-factura',
@@ -13,18 +14,29 @@ export class VisualizarFacturaComponent implements OnInit {
   factura: any;
 
   constructor(private route: ActivatedRoute,
-              private facturasService: FacturasService) { }
+              private facturasService: FacturasService,
+              private numerosService: NumerosService) { }
 
   ngOnInit() {
     this.id = this.route.snapshot.params.id;
     this.facturasService.getFactura(this.id)
               .subscribe((res:any)=>{
                   this.factura = res.factura;
-                  console.log(this.factura);
+                  let importeIVA = this.numerosService.getRedond(this.factura.base * this.factura.tipo, 2);
+                  let total = this.numerosService.getRedond(this.factura.base + importeIVA, 2);
+                  this.factura.base = this.numerosService.getFormat(this.factura.base, 2);
+                  this.factura.importeIVA = this.numerosService.getFormat(importeIVA, 2);
+                  this.factura.total = this.numerosService.getFormat(total, 2);
                 },(err:any)=>{
                   console.log(err);
                 })
 
+  }
+
+  imprimirFra() {
+    document.title = this.factura.numero;
+    window.print();
+    document.title = 'ACME, S.A.';
   }
 
 }
