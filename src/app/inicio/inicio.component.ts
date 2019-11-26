c6a29576e23eb4b2bdff3636cf5d8ebf63ee2a76
pedrocore73 <pjimenez@corenetworks.es>
2019-11-26 20:48:35 +0100

    datos_fact

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FacturasService } from '../servicios/facturas.service';
+import { NumerosService } from '../servicios/numeros.service';
 
 @Component({
   selector: 'app-inicio',
@@ -15,8 +16,12 @@ export class InicioComponent implements OnInit {
   ultimoDiaUltimoMes: any;
   primerDiaPenultimoMes: any;
   ultimoDiaPenultimoMes: any;
+  frasMesCurso: number;
+  frasUltimoMes: number;
+  frasPenultimoMes: number;
 
-  constructor(private facturasService: FacturasService) { }
+  constructor(private facturasService: FacturasService,
+              private numerosService: NumerosService) { }
 
   ngOnInit() {
     this.loadFacturas();
@@ -27,7 +32,9 @@ export class InicioComponent implements OnInit {
     this.facturasService.getFacturas()
                 .subscribe((res:any)=>{
                     this.facturas = res.facturas;
-                    
+                    this.frasMesCurso = this.filterFras(this.primerDiaMesCurso, this.ultimoDiaMesCurso);
+                    this.frasUltimoMes = this.filterFras(this.primerDiaUltimoMes, this.ultimoDiaUltimoMes);
+                    this.frasPenultimoMes = this.filterFras(this.primerDiaPenultimoMes, this.ultimoDiaPenultimoMes);
                   },(err:any)=>{
                     console.log(err);
                   })
@@ -55,12 +62,6 @@ export class InicioComponent implements OnInit {
     this.ultimoDiaUltimoMes = this.getDias(ultimoMes, anyoUltimoMes)[1];
     this.primerDiaPenultimoMes = this.getDias(penultimoMes, anyoPenultimoMes)[0];
     this.ultimoDiaPenultimoMes = this.getDias(penultimoMes, anyoPenultimoMes)[1];
-    console.log(this.primerDiaMesCurso, 
-                this.ultimoDiaMesCurso,
-                this.primerDiaUltimoMes,
-                this.ultimoDiaUltimoMes,
-                this.primerDiaPenultimoMes,
-                this.ultimoDiaPenultimoMes);
   }
   
   getDias(mes, anyo){
@@ -71,6 +72,66 @@ export class InicioComponent implements OnInit {
       dia.setDate(dia.getDate() + 1);
     }
     return [dias[0],dias[dias.length - 1]];
+  }
+
+  filterFras(primerDia, ultimoDia) {
+    let sumaFacturas = 0;
+    this.facturas.forEach(factura =>{
+      if(new Date(factura.fecha).getTime() >=  primerDia.getTime() &&
+         new Date(factura.fecha).getTime() < (ultimoDia.getTime() + 24 * 60 * 60 * 1000)) {
+            sumaFacturas += factura.base;
+         }
+    });
+    return sumaFacturas;
+  }
+
+  getMes(dia){
+    let mes;
+    switch (dia.getMonth()) {
+      case 0:
+        mes = 'Enero ' + dia.getFullYear();
+        break;
+      case 1:
+        mes = 'Febrero ' + dia.getFullYear();
+        break;
+      case 2:
+        mes = 'Marzo ' + dia.getFullYear();
+        break;
+      case 3:
+        mes = 'Abril ' + dia.getFullYear();
+        break;
+      case 4:
+        mes = 'Mayo ' + dia.getFullYear();
+        break;
+      case 5:
+        mes = 'Junio ' + dia.getFullYear();
+        break;
+      case 6:
+        mes = 'Julio ' + dia.getFullYear();
+        break;
+      case 7:
+        mes = 'Agosto ' + dia.getFullYear();
+        break;
+      case 8:
+        mes = 'Septiembre ' + dia.getFullYear();
+        break;
+      case 9:
+        mes = 'Octubre ' + dia.getFullYear();
+        break;
+      case 10:
+        mes = 'Noviembre ' + dia.getFullYear();
+        break;
+      case 11:
+        mes = 'Diciembre ' + dia.getFullYear();
+        break;
+    }
+    return mes;
+  }
+
+  formatImporte(importeMes) {
+    let importeFormat = this.numerosService.getRedond(importeMes, 2);
+    importeFormat = this.numerosService.getFormat(importeFormat, 2);
+    return importeFormat;
   }
 
 }
